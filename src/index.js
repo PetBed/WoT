87eff1502dbfcab9adaf4ad6a2ff89521fcaec4a
PetBed <71561167+PetBed@users.noreply.github.com>
2025-06-13 10:41:08 +0800

    Updated CORS

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     'https://petbed.github.io',
     'http://127.0.0.1:5501',
     'http://127.0.0.1:5500/index.html',
-    'https://fcgh4w.csb.app/'
+    'https://fcgh4w.csb.app'
   ];
   const origin = req.headers.origin;
   
